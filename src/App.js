cc339085198c9d36de9d63cb6116fce0f691436b
majaokholm <majaokholm@gmail.com>
2020-01-17 13:41:17 +0100

    added jokes

--- src/App.js
+++ src/App.js
@@ -12,24 +12,47 @@ import Home from './components/Home.js';
 import Resume from './components/resume/Resume.js';
 import Footer from './components/Footer.js';
 
-class App extends Component {    
-    render() {
-        return(
-          <div>
-            <HashRouter >              
-              <NavigationBar/>
-              <div>
-                <Switch>               
-                  <Route path="/sillyness" component={Sillyness}/>
-                  <Route path="/about" component={About}/>  
-                  <Route path="/resume" component={Resume}/>                                  
-                  <Route path="/" component={Home}/>
-                </Switch>
-              </div>
-            </HashRouter>
-            <Footer/>
-            </div>            
-        );
+class App extends Component {  
+  constructor() {
+    super();
+    this.state = {
+      jokes: [
+        {
+          setup: "What time did the man go to the dentist?",
+          punchline: "Tooth hurt-y"
+        },
+        {
+          setup: "Did you hear about the guy who invented Lifesavers?",
+          punchline: "They say he made a mint"
+        },
+        {
+          setup: "Why do chicken coops only have two doors?",
+          punchline: "Because if they had four, they would be chicken sedans!"
+        },
+        {
+          setup: "Why did the Clydesdale give the pony a glass of water?",
+          punchline: "Because he was a little horse!"
+        }
+      ]
+    }
+  }  
+  render() {
+      return(
+        <div>
+          <HashRouter >              
+            <NavigationBar/>
+            <div>
+              <Switch>               
+                <Route path="/sillyness" component={() => <Sillyness humor={this.state.jokes} />}/>
+                <Route path="/about" component={About}/>  
+                <Route path="/resume" component={Resume}/>                                  
+                <Route path="/" component={Home}/>
+              </Switch>
+            </div>
+          </HashRouter>
+          <Footer/>
+          </div>            
+      );
     }
     toggle() {
         this.setState({
--- src/components/sillyness/Sillyness.js
+++ src/components/sillyness/Sillyness.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import FunCard from './FunCard.js';
 import gif from './giphy.gif';
 import './Sillyness.css'
 
-class Sillyness extends React.Component {
-    render() {
-      return (
-        <div className="container">          
-            <FunCard color={{backgroundColor: 'yellow'}}/>
-            <FunCard/>
-            <FunCard/>
-            <FunCard/>
-            <FunCard/>
-            <FunCard/>
-            <img className="gif" src={gif} alt=""/>
-        </div>
-      );
+class Sillyness extends React.Component {  
+  constructor(props) {
+    super(props);
+    this.state = {index: 0};    
+  }
+  getJoke = () => {          
+    if(this.state.index < this.props.humor.length-1){
+      this.setState({index: this.state.index + 1})
     }
+    else{
+      this.setState({index: 0})
+    }        
+  }
+  render() {
+    return (
+      <div className="container">            
+        
+        <div className="innerContainer">
+          <h1>Ready to laugh?</h1>   
+          <button type="button" className="btn btn-secondary" data-toggle="modal" data-target="#modal" onClick={this.getJoke}>Bring it on</button> 
+        </div>
+        
+        <div className="modal fade" id="modal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle">{this.props.humor[this.state.index].setup}</h5>
+                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                {this.props.humor[this.state.index].punchline}
+              </div>
+            </div>
+          </div>
+        </div> 
+        <img className="gif" src={gif} alt=""/>
+      </div>
+    );    
+  }
 }
-
 export default Sillyness;
