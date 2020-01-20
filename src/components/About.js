1403cf8a5a615fbd05dc1fa655ae4863d4a31272
majaokholm <majaokholm@gmail.com>
2020-01-20 09:53:57 +0100

    compressed background image

--- src/components/About.js
+++ src/components/About.js
@@ -6,19 +6,22 @@ import './About.css';
 class About extends React.Component {
     render() {
       return (
-        <div className="horizontal">
-          <img className="smily" src={smily} alt="fuckup"/>      
-            <ul>
-              <li>Lorem ipsum dolor sit amet, at tincidunt sadipscing temporibus pro</li>
-              <li>Lorem ipsum sed fierent intellegat an</li>
-              <li>Hinc ipsum congue ea mel</li>
-              <li>Cu nusquam sensibus efficiendi ius, ut cetero invenire pro</li>
-              <li>Hinc ipsum congue ea mel. Cu nusquam sensibus efficiendi ius, ut cetero invenire pro, id viris propriae scriptorem usu. </li>
-            </ul>
-          {/* 
-          <p>This is  a placeholder....
-              Lorem ipsum dolor sit amet, at tincidunt sadipscing temporibus pro, sed fierent intellegat an. Hinc ipsum congue ea mel. Cu nusquam sensibus efficiendi ius, ut cetero invenire pro, id viris propriae scriptorem usu. In ius postulant reprimique, cu pro vitae urbanitas deterruisset, nec dicunt facilisis ei. Eu vim iuvaret intellegat. Per eu apeirian iudicabit rationibus, est ei tantas sensibus scribentur, partem persecuti honestatis ex mei.
-          </p>           */}
+        <div className="contentAbout">
+          <h1>Who is 'Maja'?</h1>          
+          <div className="horizontal">            
+            <img className="smily" src={smily} alt="fuckup"/>      
+              <ul>
+                <li>Lorem ipsum dolor sit amet, at tincidunt sadipscing temporibus pro</li>
+                <li>Lorem ipsum sed fierent intellegat an</li>
+                <li>Hinc ipsum congue ea mel</li>
+                <li>Cu nusquam sensibus efficiendi ius, ut cetero invenire pro</li>
+                <li>Hinc ipsum congue ea mel. Cu nusquam sensibus efficiendi ius, ut cetero invenire pro, id viris propriae scriptorem usu. </li>
+              </ul>
+            {/* 
+            <p>This is  a placeholder....
+                Lorem ipsum dolor sit amet, at tincidunt sadipscing temporibus pro, sed fierent intellegat an. Hinc ipsum congue ea mel. Cu nusquam sensibus efficiendi ius, ut cetero invenire pro, id viris propriae scriptorem usu. In ius postulant reprimique, cu pro vitae urbanitas deterruisset, nec dicunt facilisis ei. Eu vim iuvaret intellegat. Per eu apeirian iudicabit rationibus, est ei tantas sensibus scribentur, partem persecuti honestatis ex mei.
+            </p>           */}
+          </div>
         </div>
       );
     }
