bf11922b0cc2879f86e522f068eb7da36632947f
majaokholm <majaokholm@gmail.com>
2020-01-24 15:26:46 +0100

    added chuck feature'

--- src/components/sillyness/Sillyness.js
+++ src/components/sillyness/Sillyness.js
@@ -2,6 +2,8 @@ import React from 'react';
 import gif from './giphy.gif';
 import './Sillyness.css'
 
+const chuckGifs = ['https://media.giphy.com/media/l1J3Tqz2fpsx60MDe/giphy.gif','https://media.giphy.com/media/l1J3Tqz2fpsx60MDe/giphy.gif', 'https://media.giphy.com/media/AhwuGl0MPNzyg/giphy.gif', 'https://media.giphy.com/media/K2ZWOSOLLYsAE/giphy.gif', 'https://media.giphy.com/media/l1J3KhGhb1E9vqWQ0/giphy.gif', 'https://media.giphy.com/media/L3cUQlPbQMrwQ/giphy.gif', 'https://media.giphy.com/media/l1J3AS8RShMebsmgU/giphy.gif'] 
+
 class Sillyness extends React.Component {  
   constructor(props) {
     super(props);
@@ -9,24 +11,25 @@ class Sillyness extends React.Component {
       error: null,
       isLoaded: false,
       joke: {
-        "id": "R7UfaahVfFd",
-        "joke": "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
-        "status": 200
-      }
+        "icon_url" : "https://assets.chucknorris.host/img/avatar/chuck-norris.png",
+        "id" : "2mMtMv0NTWaRikTTIotRHA",
+        "url" : "",
+        "value" : "When Chuck Norris sneezes the rest of the world has 1/16th of an orgasm."
+      },
+      index:0
     };
   }
   getJoke = () => {    
-    fetch('https://icanhazdadjoke.com/', {
-      headers: {
-      'Content-type': 'application/json'
-      }})
+    fetch('https://api.chucknorris.io/jokes/random')
       .then(res => res.json())      
       .then(
-        (result) => {
+        (result) => {          
           this.setState({
             isLoaded: true,
-            joke: result.items
+            joke: result, 
+            index: Math.floor((Math.random() * chuckGifs.length))
           });
+          console.log(this.state.index)
         },
         (error) => {
           this.setState({
@@ -37,24 +40,29 @@ class Sillyness extends React.Component {
         }
       )
   }
+
   componentDidMount() {    
-    this.getJoke();
+    this.getJoke();    
   }
 
-  render() {
+  render() {    
     return (
       <div className="container">            
         
         <div className="innerContainer">
-          <h1 className="AmaticSCFont">Ready to laugh?</h1>   
-          <button type="button" className="buttonStyle" data-toggle="modal" data-target="#modal" onClick={this.getJoke}>Bring it on</button> 
+          <h1 className="AmaticSCFont">Whats up with Chuck?</h1>   
+          <button type="button" className="buttonStyle" data-toggle="modal" data-target="#modal" onClick={this.getJoke}>Let me know!</button> 
+          <div>
+            <p className="smallPrint">Brought to you by Chucknorris.io</p>
+          </div>
         </div>
         
         <div className="modal fade" id="modal" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
           <div className="modal-dialog modal-dialog-centered" role="document">
             <div className="modal-content">
               <div className="modal-body">
-                {this.state.joke.joke}
+                <img src={chuckGifs[this.state.index]} alt=""></img>
+                {this.state.joke.value}
               </div>
             </div>
           </div>
