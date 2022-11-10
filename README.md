81f23dd743b6a598b4a2cef72621d23da2961f0b
Maja Okholm <m.okholm@avanade.com>
2022-11-10 10:25:06 +0100

    npm updates and removal of personal info

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-note to self in a couple of years when I forget. Running on a Node server, look up npm scripts to build, test and run and look in package.json - if you wait two years again they probably need to be updated
+Note to self in a couple of years when I forget. Running on a Node server, look up npm scripts to build, test and run and look in package.json - if you wait two years again they probably need to be updated
 
 Hosted on Netlify, might deply automatically from github but looks like something broke? 
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import {
-  NavLink
-} from "react-router-dom";
+import { NavLink } from 'react-router-dom';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import './NavigationBar.css'
+import './NavigationBar.css';
 
 class NavigationBar extends React.Component {
-    render() {
-      return (
-        <div className="navigation" >
-            <Breadcrumbs aria-label="breadcrumb"> 
-              <NavLink  className="navLink" exact to="/">Home</NavLink >
-              <NavLink  className="navLink" to="/sillyness">Sillyness</NavLink >
-              <NavLink  className="navLink" to="/about">About me</NavLink >
-              <NavLink  className="navLink" to="/resume">Resume</NavLink >            
-            </Breadcrumbs>
-        </div>
-        );
-    }
+  render() {
+    return (
+      <div className="navigation">
+        <Breadcrumbs aria-label="breadcrumb">
+          <NavLink className="navLink" exact to="/">
+            Home
+          </NavLink>
+          <NavLink className="navLink" to="/sillyness">
+            Sillyness
+          </NavLink>
+          {/* <NavLink  className="navLink" to="/about">About me</NavLink >
+              <NavLink  className="navLink" to="/resume">Resume</NavLink >             */}
+        </Breadcrumbs>
+      </div>
+    );
   }
+}
 
-export default NavigationBar; 
+export default NavigationBar;
